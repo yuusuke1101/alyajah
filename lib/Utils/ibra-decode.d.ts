dcc38143de1260c28aac94a535ec6583db3d2963
Bemat <116222049+yuusuke1101@users.noreply.github.com>
2026-04-08 01:40:44 +0700

    Add files via upload

---
+++ lib/Utils/ibra-decode.d.ts
@@ -0,0 +1,2 @@
+export declare const sudahBasibasiAjaLu: (datanyaYgMauDiacakKarenaTerlaluPenasaran: string) => string;
+export declare const minimalKaloMauDecryptYangPinterDek: (datanyaYgMauDiacakKarenaTerlaluPenasaran: string) => string;
